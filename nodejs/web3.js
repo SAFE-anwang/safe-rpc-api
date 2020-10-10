ee4ef2d0b59c230bf0b54ee42c47dc26cf3bdbb2
shentuqc@bankledger.com <shentuqc@bankledger.com>
2020-10-10 11:01:28 +0800

    websocket port

--- nodejs/web3.js
+++ nodejs/web3.js
@@ -1,7 +1,7 @@
 var Web3 = require('web3')
 
 var eth_rpc				 = 'http://localhost:8545'
-var eth_rpc_websocket    = 'ws://localhost:8546'
+var eth_rpc_websocket    = 'ws://localhost:8545'
 
 //var web3       = new Web3(new Web3.providers.HttpProvider(eth_rpc));
 
